9963235d6678e7c93b0c89033829a5a397e8fc2f
Takahiro Sanada <sndtkrh@gmail.com>
2019-08-11 22:38:12 +0900

    fix a bug in color.ml

--- src/color.ml
+++ src/color.ml
@@ -5,6 +5,6 @@ type t = {
   }
 
 let color_convertor decrese_rate color =
-  { r = color.r * decrese_rate.r;
-    g = color.g * decrese_rate.g;
-    b = color.g * decrese_rate.b }
+  { r = color.r *. decrese_rate.r;
+    g = color.g *. decrese_rate.g;
+    b = color.g *. decrese_rate.b }
